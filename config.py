f7eeb994f1a97b4417e845b0c9c98f0b817ea2e1
Gruber <gruberlu@media-saturn.com>
2021-07-02 14:24:56 +0200

    password and token via env variables

--- config.py
+++ config.py
@@ -1,21 +1,26 @@
 from google.cloud import secretmanager
-from google.cloud.secretmanager_v1.types.resources import ReplicationStatus
+import os
 
 class Config():
     def __init__(self):
+        self.PASSWORD = os.getenv("PASSWORD", None)
+        self.API_TOKEN = os.getenv("API_TOKEN", None)
 
-        self.PROJECT_ID = "mms-msc-msc-d-fx5e"
+        if(not self.PASSWORD or not self.API_TOKEN):
+            # TBD: does not work in docker container
+            PROJECT_ID = "mms-msc-msc-d-fx5e"
 
-        client = secretmanager.SecretManagerServiceClient()
-        secret = f"projects/{self.PROJECT_ID}/secrets/"
-        version = "/versions/latest"
-        
-        # get api token
-        response = client.access_secret_version(request={"name": secret + "msc-fast-api-token" + version})
-        self.API_TOKEN = response.payload.data.decode("UTF-8")
-        
-        # get password
-        response = client.access_secret_version(request={"name": secret + "msc-fast-api-password" + version})
-        self.PASSWORD = response.payload.data.decode("UTF-8")
+            client = secretmanager.SecretManagerServiceClient()
+            secret = f"projects/{PROJECT_ID}/secrets/"
+            version = "/versions/latest"
+            
+            # get api token
+            response = client.access_secret_version(request={"name": secret + "msc-fast-api-token" + version})
+            self.API_TOKEN = response.payload.data.decode("UTF-8")
+            
+            # get password
+            response = client.access_secret_version(request={"name": secret + "msc-fast-api-password" + version})
+            self.PASSWORD = response.payload.data.decode("UTF-8")
+
 
 config = Config()
