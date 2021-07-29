70f532408b065977c651717bee1184cca8456f05
filserm-mms <filser@mediamarktsaturn.com>
2021-07-29 10:33:12 +0200

    changed startup script

--- startup_fastapi.sh
+++ startup_fastapi.sh
@@ -1,42 +1,48 @@
 #! /bin/bash
-apt-get update
-apt-get install python3-pip
 
-sudo apt-get install -y \
+apt-get update -y
+apt-get install -y python3-pip 
+
+apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
-    gnupg \
+    gnupg2 \
     lsb-release \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt-get update 
-
-apt-get install -y docker-ce docker-ce-cli containerd.io
-
-#useradd -m -g sudo -p  $(openssl passwd -crypt $PASSWORD) $USER
-
-groupadd docker
-#usermod -aG docker $USER
-
-#build fastapi app
-#docker build -t nginx-unit-fastapi .
-#docker create -p 80:80 --add-host dwh4test:172.17.133.71 --name fastapi nginx-unit-fastapi
-#docker start fastapi
+apt install -y docker-ce
 
 
-create daemon.json
- /etc/docker/daemon.json  --> and then restart daemon
+###create daemon.json --> this is needed as otherwise the routing to interconnect is not working any more
+touch  /etc/docker/daemon.json 
+cat <<EOT >> /etc/docker/daemon.json 
 {
-  "bip": "10.15.0.1/24",
-  "default-address-pools": [
-    {"base": "10.20.0.0/16", "size": 24},
-    {"base": "10.40.0.0/16", "size": 24}
-  ]
-}
+   "bip": "10.15.0.1/24",
+   "default-address-pools": [
+     {"base": "10.20.0.0/16", "size": 24},
+     {"base": "10.40.0.0/16", "size": 24}
+   ]
+ }
+EOT
+
+systemctl restart docker
+
+useradd -m -g sudo -p  $(openssl passwd -crypt $PASSWORD) fastapi
+usermod -aG docker fastapi
+
+###install github cli
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+
+###manual doing after github repo was cloned 
+###build fastapi app
+#docker build -t nginx-unit-fastapi .
+#docker create -p 80:80 --add-host dwhflash:172.17.133.32 --name fastapi nginx-unit-fastapi
+#docker start fastapi
